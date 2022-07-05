d74b7a9e061bf6e05f2b0137ca109bb75646ee37
Maxwell Dantas <mdszdev@gmail.com>
2022-07-05 09:13:50 -0300

    Updated dataDiariosSelic service to get all data according to past date, maximum limit between dates 10 years

--- src/main/kotlin/br/com/calculosinvestimentos/client/SelicDadosDiariosClient.kt
+++ src/main/kotlin/br/com/calculosinvestimentos/client/SelicDadosDiariosClient.kt
@@ -4,6 +4,7 @@ import br.com.calculosinvestimentos.dto.SelicDTO
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
 
 @FeignClient(name = "selic", url = "\${api.rest.selic}")
 interface SelicDadosDiariosClient {
@@ -12,5 +13,5 @@ interface SelicDadosDiariosClient {
         headers = ["Accept: application/json, text/plain, */*", "Content-Type: application/json"],
         produces = ["application/json"]
     )
-    fun obterTaxaDiarias(@RequestBody selicDTO: SelicDTO?): Map<String, Any>
+    fun obterTaxaDiarias(@RequestBody selicDTO: SelicDTO?, @RequestParam("page") page: String): Map<String, Any>
 }
--- src/main/kotlin/br/com/calculosinvestimentos/service/implementation/SelicServiceImpl.kt
+++ src/main/kotlin/br/com/calculosinvestimentos/service/implementation/SelicServiceImpl.kt
@@ -5,24 +5,63 @@ import br.com.calculosinvestimentos.dto.SelicDTO
 import br.com.calculosinvestimentos.service.SelicService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.text.SimpleDateFormat
 import java.time.LocalDate
+import java.time.ZoneId
 import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
+import java.util.*
 
 @Service
 class SelicServiceImpl : SelicService {
+
+    private final val dateFormatBR: String = "dd/MM/yyyy"
 
     @Autowired
     lateinit var selicDadosDiariosClient: SelicDadosDiariosClient
 
     override fun dadosDiariosSelic(selicDTO: SelicDTO?): Any {
-        if (selicDTO == null) {
+
+        var page = "1"
+
+        if (selicDTO != null) {
+            val dtInicial: Date = SimpleDateFormat(dateFormatBR).parse(selicDTO?.dataInicial)
+            val dtFinal: Date = SimpleDateFormat(dateFormatBR).parse(selicDTO?.dataFinal)
+
+            if (dtInicial > dtFinal) {
+                throw Throwable("Data inicial não pode ser maior que data final.")
+            }
+
+            val qtDiasEntreDatas = ChronoUnit.DAYS.between(
+                dtInicial.toInstant().atZone(ZoneId.systemDefault()).toLocalDate(),
+                dtFinal.toInstant().atZone(ZoneId.systemDefault()).toLocalDate()
+            )
+
+            if (qtDiasEntreDatas > 3652) {
+                throw Throwable("Intervalo entre datas deve ser menor que 10 anos.")
+            }
+
+            val listDadosDiariosSelic: LinkedHashSet<Any> = LinkedHashSet()
+            var item: Long = 1
+
+            while (item < qtDiasEntreDatas) {
+                page = item.toString()
+                val selicDadosDiarios = selicDadosDiariosClient.obterTaxaDiarias(selicDTO, page)
+                if ((selicDadosDiarios["registros"] as ArrayList<*>).size > 0) {
+                    listDadosDiariosSelic.add(selicDadosDiarios)
+                    item += 1
+                } else {
+                    item = qtDiasEntreDatas
+                }
+            }
+
+            return listDadosDiariosSelic
+        } else {
             val localDate: LocalDate = LocalDate.now()
-            val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+            val formatter = DateTimeFormatter.ofPattern(dateFormatBR)
             val data: String = formatter.format(localDate)
             val newSelicDTO = SelicDTO(data, data)
-            return selicDadosDiariosClient.obterTaxaDiarias(newSelicDTO)
+            return selicDadosDiariosClient.obterTaxaDiarias(newSelicDTO, page)
         }
-
-        return selicDadosDiariosClient.obterTaxaDiarias(selicDTO)
     }
 }
